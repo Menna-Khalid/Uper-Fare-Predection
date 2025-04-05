5d2dfe68f40e13a429c4f75406c37e4f636c38a4
Menna Khaled <166849841+Menna-Khalid@users.noreply.github.com>
2025-04-05 13:50:18 +0200

    Update settings.py

--- UberFareProject/settings.py
+++ UberFareProject/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "django-insecure-vym-in3=#pj2ct0nyb$)z(mb))lu5@#p(s(1z&4&bukx^%(tse"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
